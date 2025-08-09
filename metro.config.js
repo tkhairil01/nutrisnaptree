d4b5e831c582e90cc70769777d93e602764a8ef9
tkhairil01 khairil <t.khairil.01@gmail.com>
2025-08-09 14:32:27 +0000

    Migrate project to Expo SDK 50 and update dependencies

--- metro.config.js
+++ metro.config.js
@@ -3,8 +3,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Anda dapat menambahkan konfigurasi kustom di sini NANTI,
-// setelah build berhasil dengan konfigurasi dasar.
-// Pastikan konfigurasi kustom tersebut kompatibel dengan SDK 49.
-
 module.exports = config;
