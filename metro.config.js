787ac9c1f5968728e2cfaa3e7f1cdebdcf144fc5
tkhairil01 khairil <t.khairil.01@gmail.com>
2025-08-09 14:04:22 +0000

    Cleaned metro.config.js and reinstalled dependencies for SDK 49

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,11 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
+// metro.config.js
 const { getDefaultConfig } = require('expo/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+// Jika Anda memiliki konfigurasi khusus, tambahkan di sini.
+// Pastikan itu kompatibel dengan SDK 49.
+// Contoh:
+// config.resolver.assetExts.push('db');
 
 module.exports = config;
